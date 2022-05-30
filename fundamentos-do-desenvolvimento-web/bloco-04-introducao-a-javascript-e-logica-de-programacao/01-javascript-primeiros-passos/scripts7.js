0709cb755a7667a0a1b50397906ef1b76cb64626
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-05-30 15:56:21 -0300

    organizacao github

---
+++ fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/01-javascript-primeiros-passos/scripts7.js
@@ -0,0 +1,27 @@
+let notaPorcentagem = 79;
+let conceito = "";
+
+if (notaPorcentagem >= 90) {
+    conceito = "A";
+    console.log(conceito);
+}
+else if (notaPorcentagem >= 80) {
+    conceito = "B";
+    console.log(conceito);
+}
+else if (notaPorcentagem >= 70) {
+    conceito = "C";
+    console.log(conceito);
+}
+else if (notaPorcentagem >= 60) {
+    conceito = "D";
+    console.log(conceito);
+}
+else if (notaPorcentagem >= 50) {
+    conceito = "E";
+    console.log(conceito);
+}
+else {
+    conceito = "F";
+    console.log(F)
+}
